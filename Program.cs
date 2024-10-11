794e5f8e6a1b693ca73b8a6051d0ece6cb673850
vecseizsolt <vecseizs@kkszki.hu>
2024-10-11 11:12:18 +0200

    5 véletlen szám létrehozása egy listába kész.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Min_Max
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> randomszamok = new List<int>();
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int randomszam = random.Next();
+                randomszamok.Add(randomszam);
+            }
+
+            foreach (int number in randomszamok)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
