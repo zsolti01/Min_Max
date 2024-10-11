fcd37bd808ffbf978ecdbb6f23dc550cf01ddb3f
vecseizsolt <vecseizs@kkszki.hu>
2024-10-11 11:43:42 +0200

    A projekt elkészült.

--- Program.cs
+++ Program.cs
@@ -14,16 +14,33 @@ namespace Min_Max
             List<int> randomszamok = new List<int>();
             Random random = new Random();
 
+            Console.WriteLine();
+
             for (int i = 0; i < 5; i++)
             {
-                int randomszam = random.Next();
+                int randomszam = random.Next(1, 101);
                 randomszamok.Add(randomszam);
             }
 
-            foreach (int number in randomszamok)
+            foreach (int szam in randomszamok)
             {
-                Console.WriteLine(number);
+                Console.WriteLine("A számok: {0}", szam);
             }
+
+            Console.WriteLine();
+
+            int legkisebb = randomszamok[0];
+            int legnagyobb = randomszamok[0];
+
+            foreach (int i in randomszamok)
+            {
+                if (i < legkisebb) legkisebb = i;
+                if (i > legnagyobb) legnagyobb = i;
+            }
+
+            Console.WriteLine("A legkisebb szám: {0}", legkisebb);
+            Console.WriteLine("A legnagyobb szám: {0}", legnagyobb);
+
         }
     }
 }
